d99ab493e3e8abc15f4d4cbb46d588761c417b65
Male Michael <malmike21@outlook.com>
2017-04-19 00:12:50 +0300

    Added function that returns the min and max value of a list

--- MinAndMaxNumber/app/find_max_min.py
+++ MinAndMaxNumber/app/find_max_min.py
@@ -1,2 +1,16 @@
 def find_max_min(numbers):
-    pass
+    if isinstance(numbers, list):
+        numbers_types = (int, float, complex)
+        if all(isinstance(x, numbers_types) for x in numbers):
+            numbers.sort()
+            if numbers[0] is numbers[len(numbers)-1]:
+                print ([len(numbers)])
+                return [len(numbers)]
+            else:
+                min_max = [numbers[0], numbers[len(numbers)-1]]
+                print (min_max)
+                return min_max
+        else:
+            print ('List contains non integers')
+    else:
+        raise TypeError
