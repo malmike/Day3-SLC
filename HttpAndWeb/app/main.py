ca307e3b1fd3fdbe2cbaf252994111647a391ccf
Male Michael <malmike21@outlook.com>
2017-04-19 16:45:06 +0300

    Made changes to the print out of the return value

--- HttpAndWeb/app/main.py
+++ HttpAndWeb/app/main.py
@@ -3,8 +3,9 @@ from client import ConsumingPublicApi
 def main():
     client = ConsumingPublicApi()
     git_hub_user_name = input('Enter your git hub user name: ')
-    value = client.get_api_data('%s'%git_hub_user_name)
-    print (value)
+    dict_value = client.get_api_data('%s'%git_hub_user_name)
+    for i in dict_value:
+        print (str(i) +'\t-\t'+ str(dict_value[i]))
 
 if __name__ == "__main__":
     main()
