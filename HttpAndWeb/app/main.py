450d1853cf8efa79bb14220b12b48dc7ada703f9
Male Michael <malmike21@outlook.com>
2017-04-19 17:49:44 +0300

    Added comments to main file

--- HttpAndWeb/app/main.py
+++ HttpAndWeb/app/main.py
@@ -1,9 +1,13 @@
 from client import ConsumingPublicApi
 
 def main():
+    #Initial an object of the ConsumingPublicApi class
     client = ConsumingPublicApi()
+    #Get the user name of the git hub account of the user
     git_hub_user_name = input('Enter your git hub user name: ')
+    #Pass the user name as an argument to the function get_api_data
     dict_value = client.get_api_data('%s'%git_hub_user_name)
+    #Loop through the values returned while printing them
     for i in dict_value:
         print (str(i) +'\t-\t'+ str(dict_value[i]))
 
